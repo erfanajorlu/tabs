d67e3bd7b8ae0e91887cae0a6761969bb5cb2d26
erfan <erfanajoglu098@gmail.com>
2023-11-11 16:56:16 +0330

    add react icon and fetchjobs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return (<></>
-  );
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  async function fetchJobs() {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (loading) {
+    return (
+      <section className="flex w-full min-h-screen justify-center items-center">
+        <h1 className="text-4xl font-extrabold">Loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
+  return <section className="flex w-full min-h-screen justify-center items-center">
+    <div className="flex flex-col  items-center">
+      <h1 className="text-4xl font-extrabold m-4">Experience</h1>
+      <div className="w-20 h-1 bg-teal-600"></div>
+    </div>
+  </section>;
 }
 
 export default App;
